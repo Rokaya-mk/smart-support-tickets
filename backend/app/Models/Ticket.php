9c80b2dd4c38d4cfcd5fd785ed096d751e2df09e
Rokaya-mk <mbairikrqia@gmail.com>
2025-12-24 15:01:52 +0100

    filament packages + setup migrations & models

---
+++ backend/app/Models/Ticket.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ticket extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'subject',
+        'status',
+        'priority',
+    ];
+
+    public function user(){
+
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function agent(){
+        return $this->belongsTo(User::class,'assigned_to');
+    }
+}
